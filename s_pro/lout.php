9b72f80bcfd1db5740bcfd67cefde83b3b786dc4
shouvik banerjee <banerjeeshouvik29@gmail.com>
2025-05-09 09:18:24 +0530

    changes logout destination file of service provider dashboard

--- s_pro/lout.php
+++ s_pro/lout.php
@@ -2,5 +2,5 @@
 
     session_start();
     session_destroy();
-    header('location:/project_php/serviceHub/Signup_login/login.php');
+    header('location:/serviceHub/Signup_login/login.php');
 ?>
