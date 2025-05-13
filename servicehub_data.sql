01fa500677fa1e482ac1410d2af7371468d5a59b
shouvik banerjee <banerjeeshouvik29@gmail.com>
2025-05-13 10:18:01 +0530

    datbase update

--- servicehub_data.sql
+++ servicehub_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2025 at 04:39 PM
+-- Generation Time: May 13, 2025 at 06:46 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -64,17 +64,17 @@ CREATE TABLE `booking` (
   `payment_status` enum('pending','success','failed','refunded') NOT NULL,
   `transaction_id` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `approved_status` enum('pending','approved','rejected') NOT NULL,
-  `booking_no` int(11) NOT NULL
+  `booking_no` int(11) NOT NULL,
+  `reason` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`booking_id`, `user_id`, `service_id`, `provider_id`, `booking_status`, `booking_time`, `amount`, `payment_method`, `payment_status`, `transaction_id`, `created_at`, `approved_status`, `booking_no`) VALUES
-(3, 6, 27, 14, 'accepted', '2025-05-05 09:34:10', 2000, 'cash', 'pending', '1111111', '2025-05-05 07:36:35', 'pending', 5555),
-(4, 9, 26, 15, 'accepted', '2025-05-05 09:34:10', 2001, 'paypal', 'pending', '555', '2025-05-05 07:36:35', 'pending', 6666);
+INSERT INTO `booking` (`booking_id`, `user_id`, `service_id`, `provider_id`, `booking_status`, `booking_time`, `amount`, `payment_method`, `payment_status`, `transaction_id`, `created_at`, `booking_no`, `reason`) VALUES
+(3, 6, 27, 14, 'rejected', '2025-05-05 09:34:10', 0, '', 'failed', '1111111', '2025-05-05 07:36:35', 5555, 'bad behaviour'),
+(4, 9, 26, 14, 'completed', '2025-05-05 09:34:10', 2001, 'paypal', 'success', '555', '2025-05-05 07:36:35', 6666, '');
 
 -- --------------------------------------------------------
 
@@ -135,12 +135,12 @@ CREATE TABLE `service` (
 --
 
 INSERT INTO `service` (`service_id`, `service_name`, `image`) VALUES
-(24, 'Electrician', '6b5c5d8f515e9fa4205182b3dd1e1766.jpg'),
-(25, 'Cleaning', '02ec059100dfe256d44d50ef555591e8.jpg'),
+(24, 'Electrician', 'b1fa214830d20693ddb1cf357a4b0146.jpg'),
+(25, 'Cleaning', '504123278efd205df7aabb9764967ad3.jpg'),
 (26, 'AC Technician', '493b032192f9e5b0aac6e38cb86476c4.jpg'),
 (27, 'Interior Designer', '0cad8dc4e42fa830745515897bd1e7b1.jpg'),
 (28, 'Car Mechanic', 'b95e591a4378b1149c4fc8efff3e802d.jpg'),
-(29, 'Carpenter', 'ef1b971675330f2100340a494e15cf6d.jpg'),
+(29, 'Carpenter', '6091ff8ee4440af26ec9fd5c0f92fb23.jpg'),
 (30, 'Plumber', '1c6445f2df7b290de11c78245a1916db.jpg');
 
 -- --------------------------------------------------------
@@ -157,8 +157,8 @@ CREATE TABLE `service_providers` (
   `approved_action` enum('pending','approved','rejected') NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `businessname` varchar(50) NOT NULL,
-  `lisenceno` int(11) NOT NULL,
-  `identityno` int(11) NOT NULL,
+  `lisenceno` text NOT NULL,
+  `identityno` text NOT NULL,
   `identityimage` text NOT NULL,
   `email` text NOT NULL,
   `phone` int(10) DEFAULT NULL,
@@ -172,11 +172,9 @@ CREATE TABLE `service_providers` (
 --
 
 INSERT INTO `service_providers` (`provider_id`, `image`, `service_id`, `description`, `approved_action`, `created_at`, `businessname`, `lisenceno`, `identityno`, `identityimage`, `email`, `phone`, `provider_name`, `password`, `address`) VALUES
-(14, '', 27, 'We provide a good service.', 'approved', '2025-05-05 07:23:20', 'DesignX', 1111100000, 25206547, '', 'riya@gmail.com', 1234567891, 'Riya Das', 'Riya@2005', 'Burdwan'),
-(15, '', 26, 'we provide good service. very good. ', 'approved', '2025-05-05 07:23:20', 'Sony Max', 222111, 147852, '', 'sayan@gmail.com', 987452410, 'Sayan Majumdar', 'sayan@2004', 'Mameri'),
-(18, '', 24, '', 'approved', '2025-05-05 14:40:39', 'ghosh electronic', 0, 0, '', 'chinmoy@gmail.com', 2147483647, 'chinmoy ghosh', 'chinmoy@2000', 'Delhi'),
-(19, '', 28, 'We provide a good service.', 'pending', '2025-05-06 14:51:39', 'OLA', 0, 0, '', 'debu@gmail.com', 2147483647, 'Debu Banerjee', 'debu@2001', 'Burdwan'),
-(20, '', 30, 'We provide a good service. ', 'pending', '2025-05-08 02:41:53', 'Santra Hub', 0, 0, '', 'Anik@gmail.com', 2147483647, 'Anik Santra', 'Anik@2002', 'hooghly');
+(14, '', 27, 'We provide a good service.', 'approved', '2025-05-05 07:23:20', 'DesignX', '1111100000', '25206547', '', 'riya@gmail.com', 1234567891, 'Riya Das', 'Riya@2005', 'Burdwan'),
+(15, '', 26, 'we provide good service. very good. ', 'approved', '2025-05-05 07:23:20', 'Sony Max', '222111', '147852', '', 'sayan@gmail.com', 987452410, 'Sayan Majumdar', 'sayan@2004', 'Mameri'),
+(22, '', 28, '', 'approved', '2025-05-13 03:49:27', 'Santra Hub', 'A222111A', 'A111A', 'uploads/6822c141cad44_naihati ma.jpg', 'Anik@gmail.com', 2147483647, 'Anik Santra', '1234', 'Jamalpur');
 
 -- --------------------------------------------------------
 
@@ -202,7 +200,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `name`, `dob`, `email`, `password`, `phone`, `address`, `image`, `created_at`, `updated_at`) VALUES
-(6, 'shouvik banerjee', '2015-05-01', 'shouvik@gmail.com', 'shouvik@2005', '1234567890', 'Kolkata', '', '2025-05-05 07:04:49', '2025-05-05 07:04:49'),
+(6, 'shouvik banerjee', '2025-05-29', 'shouvik@gmail.com', 'shouvik@2005', '1234567890', 'Kolkata', '2025-05-13_050657_user_6.jpg', '2025-05-05 07:04:49', '2025-05-13 03:46:51'),
 (9, 'Sk Asraful', '2025-05-06', 'Asraful@gmail.com', 'Asraful@2004', '9784563210', 'Nigan', '', '2025-05-05 07:09:27', '2025-05-05 07:09:27'),
 (11, 'Soham dutta', '0000-00-00', 'soham@gmail.com', 'soham@2005', '7797472017', '', '', '2025-05-05 14:10:23', '2025-05-05 14:10:23'),
 (12, 'Sudipta Samanta', '0000-00-00', 'Sudipta@gmail.com', 'sudipta@2005', '6295069367', '', '', '2025-05-08 02:39:14', '2025-05-08 02:39:14');
@@ -293,7 +291,7 @@ ALTER TABLE `service`
 -- AUTO_INCREMENT for table `service_providers`
 --
 ALTER TABLE `service_providers`
-  MODIFY `provider_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `provider_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
